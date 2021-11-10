cd61ffddbc9338049150805f21a83944c44a17b6
Kishan Sojitara <kishansojitara@wireless-10-104-231-187.umd.edu>
2021-11-09 22:05:00 -0500

    working on the Jeopardy Class

--- game.py
+++ game.py
@@ -8,10 +8,7 @@ class Jeopardy:
 
 class JeopardyCatalog:
     def __init__(self, file):
-        self.list = []
-        history = {}
-        pop_culture = {}
-        math = {}
+        self.dictionary = {}
         
         with open(file, "r", encoding="utf-8") as f:
             for line in f:
@@ -19,18 +16,12 @@ class JeopardyCatalog:
                 line_list = line_list.split(",")
                 
                 category = line_list[0]
-                points = line_list[1]
+                descrip_points = line_list[1]
+                points = int(line_list[1])
                 question = line_list[2]
                 answer = line_list[3]
-                if category[0] == "History":
-                    history[points] = (question, answer)
-                    self.list.append(history)
-                elif category[0] == "Pop Culture":
-                    pop_culture[points] = (question, answer)
-                    self.list.append(pop_culture)
-                elif category[0] == "Math":
-                    math[points] = (question, answer)
-                    self.list.append(math)
+
+                self.dictionary[category + descrip_points] = (points, question, answer)
                 
     def get_question(self, subject, points):
         """
@@ -47,10 +38,9 @@ class JeopardyCatalog:
         if subject not in self.list:
             raise KeyError ("subject does not exist in the game!")
         else:
-            dictionary = self.list.get(subject)
+            dictionary = self.dictionary.get(subject + points)
             
-            question_answer_pair = dictionary[points]
-            question = question_answer_pair[0]
+            question = dictionary[1]
             
             return question        
     
@@ -69,10 +59,27 @@ class JeopardyCatalog:
         
         dictionary = self.list.get(subject)
             
-        question_answer_pair = dictionary[points]
-        answer = question_answer_pair[1]
+        answer = dictionary[2]
             
         return answer  
     
+    def get_points(self, subject, points):
+        """
+        gets the points from the catalog that is named
+        Args:
+            subject (string): the topic of the user has chosen to answer
+        
+        Returns:
+            the cooresponding points for the question the user has requested
+        
+        Raises:
+            KeyError: if the name of the subject is not in the catalog
+        """
+        
+        dictionary = self.list.get(subject)
+            
+        points = dictionary[0]
+            
+        return points  
 
 class MemoryGame:
