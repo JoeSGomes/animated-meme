729c06ee4c7210d0af6221c7afaebf85b9144940
Joseph Gomes <joegms@josephs-mbp.myfiosgateway.com>
2021-11-15 17:08:29 -0500

    function

--- Joe_check-in1.py
+++ Joe_check-in1.py
@@ -1,7 +1,37 @@
-def available_questions(self, subject):
-        place = self.dictionary.get(subject)
-        keys = place.keys()
-        l = []
-        for i in keys:
-            l.append(i)
-        return f'{subject} Questions Available: {l}'
+def available_questions(subject):
+    """[summary]
+
+    Args:
+        subject ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    dictionary = {'Pop Culture': {500: ('How many kids does Angelina Jolie have?', '6'),  
+                                  300: ('How many fast and furious movies are there?', '9'), 
+                                  200: ('Who is Jay-Z’s wife?', 'Beyonce'), 
+                                  100: ('What is Justin Bieber’s top song?', 'baby')}, 
+                  
+              'Math': {500: ('What is the derivative of 60x^2', '120'), 
+                       400: ('What is (40^2) x 8', '12800'), 
+                       300: ('What is (25000/5) x 2', '10000'), 
+                       100: ('What is 10^2?', '100')}, 
+              
+              'History': {500: ('Who invented the first electric automobile? (no spaces)', 'robert anderson'), 
+                          400: ('What year did the american revolution start', '1775'),  
+                          200: ('What year did Christopher Columbus discover America?', '1492'), 
+                          100: ('Who gave the statue of liberty to the united states', 'France')}}
+    place = dictionary.get(subject)
+    keys = place.keys()
+    l = []
+    for i in keys:
+        l.append(i)
+    return f'{subject} Questions Available: {l}'
+
+print()
+print (available_questions("Math"))
+print()
+print (available_questions("History")) 
+print()
+print (available_questions("Pop Culture"))
+print()
