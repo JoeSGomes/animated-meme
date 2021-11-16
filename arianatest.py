1b66d947d35408af0d3b7351f60d086f3eebe84f
arisan0 <73265443+arisan0@users.noreply.github.com>
2021-11-15 21:16:06 -0500

    Update arianatest.py

--- arianatest.py
+++ arianatest.py
@@ -40,14 +40,14 @@ class MemoryGame:
               words_tally = words_tally - 1
               print("Sorry, that was incorrect, please try again!")
               print(f'You have {words_tally} questions left')
-              wa = input("What was the first word? ")
+              wa = input("What was the word? ")
               
       if wa.lower() == self.words[i]:
           print("You got it right! Onto the next word...")
 
 
                 
-words = "apple", "orange", "banana", "tomato", "grape"
+words = ["apple", "orange", "banana", "tomato", "grape"]
 q1 = "What is  the first element in the periodic table? "
 a1 = "hydrogen"
 q2 = "What is the last letter of the alphabet? "
@@ -56,8 +56,8 @@ q3 = "Who was the second president of the United States? "
 a3 = "john adams"
 q4 = "What shape is generally used for stop signs? "
 a4 = "octagon"
-q5 = "How many continents are there on Earth? "
-a5 = "seven"
+q5 = "Who won the 2021 World Series? "
+a5 = "braves"
 
 mg = MemoryGame(words, interval)
 
@@ -66,17 +66,17 @@ question_set1 = mg.questions(0, q1, a1)
 print(question_set1)
 
 print(mg.display_words(1))
-question_set2 = mg.questions(0, q2, a2)
+question_set2 = mg.questions(1, q2, a2)
 print(question_set2)
 
 print(mg.display_words(2))
-question_set3 = mg.questions(0, q3, a3)
+question_set3 = mg.questions(2, q3, a3)
 print(question_set3)
 
 print(mg.display_words(3))
-question_set4 = mg.questions(0, q4, a4)
+question_set4 = mg.questions(3, q4, a4)
 print(question_set4)
 
 print(mg.display_words(4))
-question_set1 = mg.questions(0, q5, a5)
+question_set5 = mg.questions(4, q5, a5)
 print(question_set5)
