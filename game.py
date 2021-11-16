0d2b23bc090fd63a93c4589340556186bec7b22a
Walter Gonzalez <waltzalez@yahoo.com>
2021-11-15 20:31:00 -0500

    docstrings for def hints

--- game.py
+++ game.py
@@ -288,6 +288,14 @@ class GuessNumber:
             print("You ran out of tries; better luck next time!")
     
     def hints(counter, number): 
+        '''this method gives 3 separate hints for player if requested
+        
+        Args: 
+            counter(int): counter starts at 0, each time player prompts 'hint' 1 is added to counter to
+                          total 3 at the end.
+            number(int): the random number generated, will be added or subtracted or added by hint
+        
+        '''
 	
         if counter ==  1: 
             print (f"Number is between {number - 20} and {number + 20}")
