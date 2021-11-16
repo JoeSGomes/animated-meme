e25d32413b8b064bd8e68a81adc3db1d1c1ef531
arisan0 <73265443+arisan0@users.noreply.github.com>
2021-11-15 19:37:45 -0500

    Update ariana_check-in1.py

--- ariana_check-in1.py
+++ ariana_check-in1.py
@@ -71,7 +71,7 @@ class MemoryGame:
         words_tally = 3
         while questions_tally != 0:
             qa1 = input("What is  the first element in the periodic table? ")
-            if qa1 == "Hydrogen" or qa1 == "hydrogen":
+            if qa1.lower() == "Hydrogen":
                 wa1 = input("What was the first word? ")
                 if wa1 == self.words[0]:
                     print("Correct!")
@@ -84,7 +84,7 @@ class MemoryGame:
                 print(f"You have {questions_tally} left")
                 
             qa2 = input("What is 6/(2(1+2))?")
-            if qa2 == 1:
+            if qa2 == 1 or qa2.lower() == "One":
                 wa2 = input("What was the second word? ")
                 if wa2 == self.words[1]:
                     print("Correct!")
@@ -97,7 +97,7 @@ class MemoryGame:
                 print(f"You have {questions_tally} left")
                 
             qa3 = input("Who was the second president of the United States? ")
-            if qa3 == "John Adams" or qa3 == "john adams":
+            if qa3.lower() == "john adams":
                 wa3 = input("What was the third word? ")
                 if wa3 == self.words[2]:
                     print("Correct!")
@@ -110,7 +110,7 @@ class MemoryGame:
                 print(f"You have {questions_tally} left")
             
             qa4 = input("What shape is generally used for stop signs? ")
-            if qa4 == "Octagon" or qa4 == "octagon":
+            if qa4.lower() == "Octagon":
                 wa4 = input("What was the fourth word? ")
                 if wa4 == self.words[3]:
                     print("Correct!")
@@ -123,7 +123,7 @@ class MemoryGame:
                 print(f"You have {questions_tally} left")
                 
             qa5 = input("How many continents are there on Earth? ")
-            if qa5 == 7 or qa5 == "Seven" or qa5 == "seven":
+            if qa5 == 7 or qa5.lower() == "Seven":
                 wa5 = input("What was the fifth word? ")
                 if wa5 == self.words[4]:
                     print("Correct!")
