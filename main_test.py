86c1b4519390e508835e6ebf35d746f0e05f5f66
Walter Gonzalez <waltzalez@yahoo.com>
2021-11-22 20:37:31 -0500

    test

--- main_test.py
+++ main_test.py
@@ -1,24 +1,28 @@
 def main():
     while games_won = 0
     games_attempted = 0 
+    games=['memory game', 'jeopardy', 'guess the number']
     
     while games_won < 2 and games_attempted <= 3:
-        choice = input('Which game would you like to play? /n Jeopardy , Memory Game , Guess the number?')
+        choice = input(f'Which game would you like to play? /n {games}').lower()
         
-        if choice == "Jeopardy": 
+        if choice == "jeopardy": 
             jg = Jeopardy(0)
             print(jg.play_jeopardy_game())
             games_attempted += 1 
+            games('jeopardy').remove()
             
-        elif choice == "Memory Game": 
+        elif choice == "memory game": 
             mg = MemoryGame(.5)
             print(mg.mechanics())
             games_attempted += 1 
+            games('memory game').remove()
         
-        elif choice == "Guess the Number": 
+        elif choice == "guess the number": 
             gg = GuessTheNumber()
             print(gg.mechanics_of_game())
-            games_attempted += 1 
+            games_attempted += 1
+            games('guess the number').remove()
         
         else: 
             raise ValueError ("Sorry, that is not a valid game, try again!")
